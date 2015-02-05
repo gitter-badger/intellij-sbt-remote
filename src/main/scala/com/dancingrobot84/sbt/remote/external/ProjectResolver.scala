cf0eb238d85387cfce2920874963fabaf7c14c11
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-02-05 17:42:16 +0300

    Implement connection test

--- src/main/scala/com/dancingrobot84/sbt/remote/external/ProjectResolver.scala
+++ src/main/scala/com/dancingrobot84/sbt/remote/external/ProjectResolver.scala
@@ -1,9 +1,17 @@
 package com.dancingrobot84.sbt.remote.external
 
+import java.io.File
+
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.project.ProjectData
-import com.intellij.openapi.externalSystem.model.task.{ExternalSystemTaskId, ExternalSystemTaskNotificationListener}
+import com.intellij.openapi.externalSystem.model.task.{ExternalSystemTaskId, ExternalSystemTaskNotificationEvent, ExternalSystemTaskNotificationListener}
 import com.intellij.openapi.externalSystem.service.project.ExternalSystemProjectResolver
+import sbt.client.SbtConnector
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Promise}
+import scala.util.Success
 
 /**
  * @author Nikolay Obedin
@@ -16,7 +24,27 @@ class ProjectResolver
                          projectPath: String,
                          isPreview: Boolean,
                          settings: ExecutionSettings,
-                         listener: ExternalSystemTaskNotificationListener): DataNode[ProjectData] = null
+                         listener: ExternalSystemTaskNotificationListener): DataNode[ProjectData] = {
+    val connector = SbtConnector(configName = "idea",
+                                 humanReadableName = "Intellij IDEA",
+                                 directory = new File(projectPath))
+
+    listener.onStatusChange(new ExternalSystemTaskNotificationEvent(id, "Connecting to SBT server..."))
+    val p = Promise[DataNode[ProjectData]]()
+
+    connector.open({ client =>
+      listener.onStatusChange(new ExternalSystemTaskNotificationEvent(id, "OK, connected, closing..."))
+      Thread.sleep(2000)
+      client.close()
+      p.complete(Success(null))
+    }, { (reconnecting, message) =>
+      listener.onStatusChange(new ExternalSystemTaskNotificationEvent(id, message))
+      Thread.sleep(2000)
+      p.complete(Success(null))
+    })
+
+    Await.result(p.future, Duration.Inf)
+  }
 
   def cancelTask(id: ExternalSystemTaskId,
                  listener: ExternalSystemTaskNotificationListener) = false
