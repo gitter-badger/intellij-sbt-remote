e90e05d9798d90253f8a22a61eb92a569b385d98
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-02-05 12:23:43 +0300

    Add icon and bundle

---
+++ src/main/scala/com/dancingrobot84/sbt/remote/Bundle.scala
@@ -0,0 +1,27 @@
+package com.dancingrobot84.sbt.remote
+
+import java.lang.ref.{Reference, SoftReference}
+import java.util.ResourceBundle
+
+import com.intellij.CommonBundle
+
+/**
+ * @author Nikolay Obedin
+ * @since 2/5/15.
+ */
+object Bundle {
+  def apply(key: String, params: AnyRef*) = CommonBundle.message(get(), key, params : _*)
+
+  private var ourBundle: Reference[ResourceBundle] = null
+  private val BUNDLE = "com.dancingrobot84.sbt.remote.Bundle"
+
+  private def get(): ResourceBundle = {
+    var bundle: ResourceBundle = null
+    if (ourBundle != null) bundle = ourBundle.get
+    if (bundle == null) {
+      bundle = ResourceBundle.getBundle(BUNDLE)
+      ourBundle = new SoftReference[ResourceBundle](bundle)
+    }
+    bundle
+  }
+}
