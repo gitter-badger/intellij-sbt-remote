49ae629e2b0a5b143914a42e58bfd841d3725334
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-04-02 19:22:05 +0300

    Implement commands execution

--- src/main/scala/com/dancingrobot84/sbt/remote/console/ConsoleToolWindowFactory.scala
+++ src/main/scala/com/dancingrobot84/sbt/remote/console/ConsoleToolWindowFactory.scala
@@ -1,13 +1,24 @@
-package com.dancingrobot84.sbt.remote.console
+package com.dancingrobot84.sbt.remote
+package console
+
+import java.awt.BorderLayout
+import java.util
+import javax.swing.JPanel
 
 import com.dancingrobot84.sbt.remote.project.components.SessionListener
 import com.dancingrobot84.sbt.remote.project.components.SessionListener.LogListener
-import com.intellij.execution.console.LanguageConsoleImpl
-import com.intellij.execution.ui.ConsoleViewContentType
+import com.intellij.execution.console.{BaseConsoleExecuteActionHandler, ConsoleExecuteAction, LanguageConsoleImpl, LanguageConsoleView}
+import com.intellij.execution.impl.ConsoleViewImpl.ClearAllAction
+import com.intellij.execution.{ ui => UI }
+import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem._
 import com.intellij.openapi.application.ex.ApplicationManagerEx
 import com.intellij.openapi.fileTypes.PlainTextLanguage
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.{ToolWindow, ToolWindowFactory, ToolWindowType}
+import com.intellij.openapi.wm.{ToolWindow, ToolWindowFactory}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.collection.JavaConverters._
 
 /**
  * @author Nikolay Obedin
@@ -15,19 +26,34 @@ import com.intellij.openapi.wm.{ToolWindow, ToolWindowFactory, ToolWindowType}
  */
 class ConsoleToolWindowFactory extends ToolWindowFactory {
   override def createToolWindowContent(project: Project, toolWindow: ToolWindow): Unit = {
-    val view = new ConsoleView(project)
-    toolWindow.getComponent.add(view.getComponent)
+    val consoleView = new ConsoleView(project)
+    val toolbarActions = new DefaultActionGroup()
+    val toolbar = ActionManager.getInstance().createActionToolbar(ActionPlaces.UNKNOWN, toolbarActions, false)
+
+    val panel = new JPanel(new BorderLayout())
+    panel.add(toolbar.getComponent, BorderLayout.WEST)
+    panel.add(consoleView.getComponent, BorderLayout.CENTER)
+
+    val execAction = new ConsoleExecuteAction(consoleView, new ConsoleExecutionHandler(project))
+    Option(execAction.getShortcutSet).foreach(ss => execAction.registerCustomShortcutSet(ss, consoleView.getComponent))
+    toolbarActions.add(execAction)
+
+    toolbar.setTargetComponent(panel)
+
+    toolWindow.getComponent.add(panel)
   }
 }
 
 class ConsoleView(project: Project) extends LanguageConsoleImpl(project, "SBT Remote REPL", PlainTextLanguage.INSTANCE) {
+  getConsoleEditor.setOneLineMode(true)
+
   SessionListener(project).foreach { listener =>
     listener.addLogListener(new LogListener {
       override def onMessage(msg: String): Unit = {
         ApplicationManagerEx.getApplicationEx.invokeLater(new Runnable {
           override def run(): Unit = {
             if (!msg.startsWith("Read from stdout:"))
-              print(msg + "\n", ConsoleViewContentType.NORMAL_OUTPUT)
+              print(msg + "\n", UI.ConsoleViewContentType.NORMAL_OUTPUT)
             scrollToEnd()
           }
         })
@@ -37,6 +63,16 @@ class ConsoleView(project: Project) extends LanguageConsoleImpl(project, "SBT Re
         if (!project.isOpen)
           dispose()
       }
+    })
+  }
+}
+
+class ConsoleExecutionHandler(project: Project) extends BaseConsoleExecuteActionHandler(false) {
+  override def execute(text: String, console: LanguageConsoleView): Unit = {
+    sbtConnectorFor(project.getBasePath).open({ client =>
+      client.requestExecution(text, None)
+    }, { (_, _) =>
+      Unit
     })
   }
 }
