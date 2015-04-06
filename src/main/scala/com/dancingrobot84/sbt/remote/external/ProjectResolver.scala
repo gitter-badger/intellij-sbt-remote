19029144095c7b65b781a8cad8fd42784b19f4c4
Nikolay Obedin <dancingrobot84@gmail.com>
2015-04-06 16:26:50 +0300

    Refactor ProjectResolver

--- src/main/scala/com/dancingrobot84/sbt/remote/external/ProjectResolver.scala
+++ src/main/scala/com/dancingrobot84/sbt/remote/external/ProjectResolver.scala
@@ -24,79 +24,98 @@ import scala.util.{ Try, Failure, Success }
 class ProjectResolver
     extends ExternalSystemProjectResolver[ExecutionSettings] {
 
-  private var projectPromise: Option[Promise[DataNode[ProjectData]]] = None
-  private var connector: Option[SbtConnector] = None
+  import ProjectResolver._
+
+  private var executor: Option[ResolutionExecutor] = None
 
   override def resolveProjectInfo(id: ExternalSystemTaskId,
                                   projectPath: String,
                                   isPreview: Boolean,
                                   settings: ExecutionSettings,
                                   listener: ExternalSystemTaskNotificationListener): DataNode[ProjectData] = {
-    connector = Some(sbtConnectorFor(projectPath))
-    projectPromise = Some(Promise())
-
     val logger = new Logger {
       def log(msg: String, level: Logger.Level, cause: Option[Throwable]): Unit =
         listener.onStatusChange(new ExternalSystemTaskNotificationEvent(id, msg))
     }
 
-      def onConnect(client: SbtClient): Unit = {
-        logger.info("Retrieving structure")
-
-        client.handleEvents {
-          case logE: LogEvent => logE.entry match {
-            case LogStdOut(_) | LogStdErr(_) | LogMessage(_, _) =>
-              logger.info(logE.entry.message)
-            case _ =>
-          }
-          case _ =>
-        }
-
-        val projectFile = new File(projectPath)
-        val projectRef = new ProjectRef {
-          var project: Project = new StatefulProject(projectFile.getCanonicalFile.toURI, projectFile.getName)
-        }
-
-        val extractors =
-          if (isPreview)
-            Seq(new InternalDependenciesExtractor with SynchronizedContext)
-          else if (settings.resolveClassifiers)
-            Seq(new InternalDependenciesExtractor with SynchronizedContext,
-              new ExternalDependenciesExtractor with SynchronizedContext,
-              new ClassifiersExtractor with SynchronizedContext)
-          else
-            Seq(new InternalDependenciesExtractor with SynchronizedContext,
-              new ExternalDependenciesExtractor with SynchronizedContext)
-
-        val extraction = for {
-          _ <- (new BasicsExtractor with SynchronizedContext).attach(client, projectRef, Log)._1
-          _ <- Future.sequence(extractors.map(_.attach(client, projectRef, Log)._1))
-          _ <- (new TasksExtractor with SynchronizedContext).attach(client, projectRef, Log)._1
-        } yield Unit
-
-        import DataNodeConversions._
-        extraction.onComplete {
-          case Success(_)   => projectPromise.foreach(_.tryComplete(Try(projectRef.project.toDataNode)))
-          case Failure(exc) => projectPromise.foreach(_.tryFailure(new ExternalSystemException(exc)))
-        }
-      }
-
-    logger.info("Connecting to SBT server")
-
-    connector.foreach(_.open(onConnect, { (reconnect, reason) =>
-      if (reconnect)
-        logger.warn(reason)
-      else
-        projectPromise.foreach(_.tryFailure(new ExternalSystemException(reason)))
-    }))
-
-    projectPromise.map(p => Await.result(p.future, Duration.Inf)).getOrElse(null)
+    executor = Some(new ResolutionExecutor(projectPath, settings, isPreview, logger))
+    executor.fold(null.asInstanceOf[DataNode[ProjectData]])(e => Await.result(e.run(), Duration.Inf))
   }
 
   override def cancelTask(id: ExternalSystemTaskId,
-                          listener: ExternalSystemTaskNotificationListener) = {
-    projectPromise.foreach(_.tryFailure(new IllegalStateException("Import cancelled")))
-    connector.foreach(_.close())
-    projectPromise.map(_.isCompleted).getOrElse(true)
+                          listener: ExternalSystemTaskNotificationListener): Boolean =
+    executor.fold(false)(_.cancel())
+}
+
+object ProjectResolver {
+  final class ResolutionExecutor(projectPath: String,
+                                 settings: ExecutionSettings,
+                                 isPreview: Boolean,
+                                 logger: Logger) {
+
+    private val projectPromise = Promise[DataNode[ProjectData]]()
+    private val cancelPromise = Promise[Unit]()
+
+    cancelPromise.future.onFailure {
+      case exc: Exception => projectPromise.tryFailure(exc)
+    }
+
+    def run(): Future[DataNode[ProjectData]] = {
+        def onConnect(client: SbtClient): Unit = {
+          client.handleEvents {
+            case logE: LogEvent => logE.entry match {
+              case LogStdOut(_) | LogStdErr(_) | LogMessage(_, _) =>
+                logger.info(logE.entry.message)
+              case _ =>
+            }
+            case _ =>
+          }
+
+          val projectFile = new File(projectPath)
+          val projectRef = new ProjectRef {
+            var project: Project = new StatefulProject(projectFile.getCanonicalFile.toURI, projectFile.getName)
+          }
+
+          val extractors =
+            if (isPreview)
+              Seq(new InternalDependenciesExtractor with SynchronizedContext)
+            else if (settings.resolveClassifiers)
+              Seq(new InternalDependenciesExtractor with SynchronizedContext,
+                new ExternalDependenciesExtractor with SynchronizedContext,
+                new ClassifiersExtractor with SynchronizedContext)
+            else
+              Seq(new InternalDependenciesExtractor with SynchronizedContext,
+                new ExternalDependenciesExtractor with SynchronizedContext)
+
+          val extraction = for {
+            _ <- (new BasicsExtractor with SynchronizedContext).attach(client, projectRef, Log)._1
+            _ <- Future.sequence(extractors.map(_.attach(client, projectRef, Log)._1))
+            _ <- (new TasksExtractor with SynchronizedContext).attach(client, projectRef, Log)._1
+          } yield Unit
+
+          import DataNodeConversions._
+          extraction.onComplete {
+            case Success(_)   => projectPromise.tryComplete(Try(projectRef.project.toDataNode))
+            case Failure(exc) => projectPromise.tryFailure(new ExternalSystemException(exc))
+          }
+        }
+
+      logger.info("Connecting to SBT server")
+
+      val subscription = sbtConnectorFor(projectPath).open(onConnect, { (reconnect, reason) =>
+        if (reconnect)
+          logger.warn(reason)
+        else
+          projectPromise.tryFailure(new ExternalSystemException(reason))
+      })
+
+      projectPromise.future.onComplete(_ => subscription.cancel())
+      projectPromise.future
+    }
+
+    def cancel(): Boolean = {
+      cancelPromise.tryFailure(new ImportCanceledException("Import cancelled"))
+      projectPromise.isCompleted
+    }
   }
 }
