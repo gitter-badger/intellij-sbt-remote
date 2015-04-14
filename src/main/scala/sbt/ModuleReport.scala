f91a7f5bacd522359ed14e16d6e6ff945d760520
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-04-14 17:06:18 +0300

    Remove unneeded information from ModuleReport

--- src/main/scala/sbt/ModuleReport.scala
+++ src/main/scala/sbt/ModuleReport.scala
@@ -6,10 +6,7 @@ import sbt.serialization._
 
 final case class ModuleReport(val module: ModuleID,
                               val artifacts: Seq[(Artifact, File)],
-                              val missingArtifacts: Seq[Artifact],
-                              val status: Option[String],
-                              val evicted: Boolean,
-                              val configurations: Seq[String])
+                              val evicted: Boolean)
 
 object ModuleReport {
   implicit val pickler: Pickler[ModuleReport] with Unpickler[ModuleReport] = PicklerUnpickler.generate[ModuleReport]
