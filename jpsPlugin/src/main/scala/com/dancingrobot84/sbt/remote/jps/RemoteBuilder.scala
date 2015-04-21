8e458c76b6a528c383da8d53227e9cc1d7ed8c7b
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-04-21 14:46:09 +0300

    Implement compilation cancellation / Fix compilation logging

--- jpsPlugin/src/main/scala/com/dancingrobot84/sbt/remote/jps/RemoteBuilder.scala
+++ jpsPlugin/src/main/scala/com/dancingrobot84/sbt/remote/jps/RemoteBuilder.scala
@@ -34,12 +34,13 @@ class RemoteBuilder extends ModuleLevelBuilder(BuilderCategory.SOURCE_GENERATOR)
                      chunk: ModuleChunk,
                      dirtyFilesHolder: DirtyFilesHolder[JavaSourceRootDescriptor, ModuleBuildTarget],
                      outputConsumer: OutputConsumer): ExitCode = {
+    implicit val implicitContext = context
     SbtRemoteProjectSettings.get(context.getProjectDescriptor.getProject).foreach { settings =>
       val donePromise = Promise[ExitCode]()
       // TODO: reuse SBT connection from IDEA process
-      context.processMessage(new ProgressMessage(Bundle("sbt.remote.jps.connectingToServer")))
+      progressMessage(Bundle("sbt.remote.jps.connectingToServer"))
       SbtConnector("idea", "Intellij IDEA", new File(settings.projectPath))
-        .open(onConnect(context, donePromise), onFailure(context, donePromise))
+        .open(onConnect(donePromise), onFailure(donePromise))
       return Await.result(donePromise.future, Duration.Inf)
     }
 
@@ -48,41 +49,84 @@ class RemoteBuilder extends ModuleLevelBuilder(BuilderCategory.SOURCE_GENERATOR)
 
   override def getPresentableName: String = Bundle("sbt.remote.jps.builderName")
 
-  def onConnect(context: CompileContext, donePromise: Promise[ExitCode])(client: SbtClient): Unit = {
+  private def onConnect(donePromise: Promise[ExitCode])(client: SbtClient)(
+      implicit context: CompileContext): Unit = {
+    checkCanceled(client, donePromise, -1)
     var jobId: Long = -1
 
-    client.handleEvents {
-      case CompilationFailure(id, failure) => failure match {
-        case CompilationFailure(_, Position(srcPath, _, lineOpt, lineContent, _, pointerOpt, space), severity, message) =>
-          val messageWithLineAndPointer =
-            message + "\n" + lineContent + space.map("\n" + _ + "^").getOrElse("")
-          val kind = severity match {
-            case xsbti.Severity.Warn  => BuildMessage.Kind.WARNING
-            case xsbti.Severity.Error => BuildMessage.Kind.ERROR
-            case _                    => BuildMessage.Kind.INFO
+    client.handleEvents { event =>
+      checkCanceled(client, donePromise, jobId)
+      event match {
+        case CompilationFailure(id, failure) => failure match {
+          case CompilationFailure(_, Position(srcPath, _, lineOpt, lineContent, _, pointerOpt, space), severity, message) =>
+            val messageWithLineAndPointer =
+              message + "\n" + lineContent + space.map("\n" + _ + "^").getOrElse("")
+            val kind = severity match {
+              case xsbti.Severity.Warn  => BuildMessage.Kind.WARNING
+              case xsbti.Severity.Error => BuildMessage.Kind.ERROR
+              case _                    => BuildMessage.Kind.INFO
+            }
+            val line = lineOpt.fold(-1L)(_.toLong)
+            val column = pointerOpt.fold(-1L)(_.toLong + 1L)
+            compilerMessage(kind, messageWithLineAndPointer, srcPath, line, column)
+          case _ => // ignore
+        }
+        case ExecutionSuccess(id) if id == jobId =>
+          donePromise.trySuccess(ExitCode.OK)
+        case ExecutionFailure(id) if id == jobId =>
+          donePromise.trySuccess(ExitCode.ABORT)
+        case TaskStarted(id, _, Some(key)) if id == jobId =>
+          progressMessage(Bundle("sbt.remote.jps.taskIsRunning", key.key.name))
+        case TaskFinished(id, _, Some(key), success, _) if id == jobId =>
+          val message = {
+            val taskName = key.key.name
+            if (success)
+              Bundle("sbt.remote.jps.taskSucceeded", taskName)
+            else
+              Bundle("sbt.remote.jps.taskFailed", taskName)
           }
-          val line = lineOpt.fold(-1L)(_.toLong)
-          val pointer = pointerOpt.fold(-1L)(_.toLong + 1L)
-          context.processMessage(new CompilerMessage(Bundle("sbt.remote.jps.compilerName"), kind,
-            messageWithLineAndPointer, srcPath.orNull, -1, -1, -1, line, pointer))
-        case _ => // do nothing
+          progressMessage(message)
+        case logEvent : LogEvent if !logEvent.entry.message.startsWith("Read from stdout:") =>
+          logEvent.entry match {
+            case LogMessage(level, message) if level != LogMessage.DEBUG =>
+              progressMessage(s"[$level] $message")
+            case _ => // ignore
+          }
+        case _ => // ignore
       }
-      case ExecutionSuccess(id) if id == jobId =>
-        donePromise.success(ExitCode.OK)
-      case ExecutionFailure(id) if id == jobId =>
-        donePromise.success(ExitCode.ABORT)
-      case x =>
-        context.processMessage(new ProgressMessage(x.toString))
     }
 
     client.requestExecution("compile", None).foreach(jobId = _)
   }
 
-  def onFailure(context: CompileContext, donePromise: Promise[ExitCode])(reconnecting: Boolean, cause: String): Unit = {
+  private def onFailure(donePromise: Promise[ExitCode])(reconnecting: Boolean, cause: String)(
+      implicit context: CompileContext): Unit = {
     context.processMessage(new BuildMessage(cause, BuildMessage.Kind.WARNING) {})
     if (!reconnecting)
-      donePromise.success(ExitCode.ABORT)
+      donePromise.trySuccess(ExitCode.ABORT)
   }
+
+  private def checkCanceled(client: SbtClient, donePromise: Promise[ExitCode], jobId: Long)(
+      implicit context: CompileContext): Unit =
+    if (context.getCancelStatus.isCanceled) {
+      if (jobId != -1) {
+        val isCancelled = !Await.result(client.cancelExecution(jobId), Duration.Inf)
+        if (!isCancelled)
+          return
+      }
+      donePromise.trySuccess(ExitCode.ABORT)
+    }
+
+  private def progressMessage(message: String)(implicit context: CompileContext): Unit =
+    context.processMessage(new ProgressMessage(message))
+
+  private def compilerMessage(kind: BuildMessage.Kind,
+                              message: String,
+                              sourcePath: Option[String],
+                              line: Long,
+                              column: Long)(implicit context: CompileContext): Unit =
+    context.processMessage(new CompilerMessage(
+      Bundle("sbt.remote.jps.compilerName"), kind, message, sourcePath.orNull, -1, -1, -1, line, column))
 }
 
 object RemoteBuilder {
